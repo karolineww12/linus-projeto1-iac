3bbe4b6eaed0c3c2791060adca33c80d27a8b9a2
Ana Karoline <111097773+karolineww12@users.noreply.github.com>
2023-08-28 15:56:43 -0300

    Update criar_user.sh
    erro passwd guest corrigido*

--- criar_user.sh
+++ criar_user.sh
@@ -6,15 +6,15 @@ useradd guest10 -c "Usuário Convidade" -s /bin/bash -m -p $(openssl passwd -cry
 passwd guest10 -e
 
 useradd guest11 -c "Usuário Convidade" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
-passwd guest10 -e
+passwd guest11 -e
 
 useradd guest12 -c "Usuário Convidade" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
-passwd guest10 -e
+passwd guest12 -e
 
 useradd guest13 -c "Usuário Convidade" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
-passwd guest10 -e
+passwd guest13 -e
 
 useradd guest14 -c "Usuário Convidade" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
-passwd guest10 -e
+passwd guest14 -e
 
 echo "Finalizado!"
